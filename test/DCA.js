627be953aa850072599a8d7490c41410bc699ec8
Wade <wz.lim.00@gmail.com>
2024-03-22 13:56:16 +0800

    xWinStrategy Token test files

---
+++ test/DCA.js
@@ -0,0 +1,72 @@
+const {
+  time,
+  loadFixture,
+} = require("@nomicfoundation/hardhat-network-helpers");
+const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
+const { expect } = require("chai");
+const { xWinFixture } = require("./xWinFixture");
+const { expectAlmostEquals } = require("./xWinTestHelpers.js");
+const { bsc, hardhatNode } = require("../bscMainnetAddresses.js");
+const { ethers } = require("hardhat");
+const defaultAmount = ethers.parseEther("10000");
+
+describe("DCA", function () {
+  describe("Core", function () {
+    it("xUSDT-xBTC", async function () {
+      const { owner, accounts, xWinDCA, USDT, BTCB } = await loadFixture(
+        xWinFixture
+      );
+      await USDT.approve(await xWinDCA.getAddress(), defaultAmount);
+      await USDT.connect(accounts[0]).approve(
+        await xWinDCA.getAddress(),
+        defaultAmount
+      );
+
+      expect(await xWinDCA.baseToken()).to.equal(bsc.USDT);
+      expectAlmostEquals(await xWinDCA.getUnitPrice(), ethers.parseEther("1"));
+      expectAlmostEquals(await xWinDCA.getVaultValues(), ethers.parseEther("0"));
+      expectAlmostEquals(
+        await xWinDCA.getVaultValuesInUSD(),
+        ethers.parseEther("0")
+      );
+
+      await xWinDCA.deposit(defaultAmount);
+
+      expectAlmostEquals(await xWinDCA.getUnitPrice(), ethers.parseEther("1"));
+      expectAlmostEquals(
+        await xWinDCA.getVaultValues(),
+        defaultAmount
+      );
+      await network.provider.send("hardhat_mine", ["0x7080"]);
+      await xWinDCA.systemDeposit();
+
+      expectAlmostEquals(await xWinDCA.getStableValues(), ethers.parseEther("9970"));
+      expectAlmostEquals(await xWinDCA.getTargetValues(), ethers.parseEther("27.7"));
+      expectAlmostEquals(await xWinDCA.getBaseValues(), ethers.parseEther("0"));
+  
+
+
+      await xWinDCA.connect(accounts[0]).deposit(defaultAmount);
+
+      expectAlmostEquals(await xWinDCA.getUnitPrice(), ethers.parseEther("1"));
+      expectAlmostEquals(
+        await xWinDCA.getVaultValues(),
+        ethers.parseEther("20000")
+      );
+
+      await network.provider.send("hardhat_mine", ["0x7080"]);
+      await xWinDCA.systemDeposit();
+      expectAlmostEquals(await xWinDCA.getStableValues(), ethers.parseEther("19910"));
+      expectAlmostEquals(await xWinDCA.getTargetValues(), ethers.parseEther("83"));
+      expectAlmostEquals(await xWinDCA.getBaseValues(), ethers.parseEther("0"));
+
+      await xWinDCA.withdraw(await xWinDCA.balanceOf(await owner.getAddress()));
+
+      expectAlmostEquals(await xWinDCA.getUnitPrice(), ethers.parseEther("1"));
+      expectAlmostEquals(
+        await xWinDCA.getVaultValues(),
+        defaultAmount
+      );
+    });
+  });
+});
---
+++ test/SingleAsset.js
@@ -0,0 +1,127 @@
+const {
+  time,
+  loadFixture,
+} = require("@nomicfoundation/hardhat-network-helpers");
+const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
+const { expect } = require("chai");
+const { xWinFixture } = require("./xWinFixture");
+const { expectAlmostEquals } = require("./xWinTestHelpers.js");
+const { bsc, hardhatNode } = require("../bscMainnetAddresses.js");
+const { ethers } = require("hardhat");
+const defaultAmount = ethers.parseEther("1000");
+const defaultAmountBTC = ethers.parseEther("0.1");
+
+describe("Single Asset", function () {
+  describe("Core", function () {
+    it("xUSDT", async function () {
+      const { owner, accounts, xUSDT, xBTCB, USDT, BTCB } = await loadFixture(
+        xWinFixture
+      );
+      await USDT.approve(await xUSDT.getAddress(), defaultAmount);
+      await USDT.connect(accounts[0]).approve(
+        await xUSDT.getAddress(),
+        defaultAmount
+      );
+
+      expect(await xUSDT.baseToken()).to.equal(bsc.USDT);
+      expectAlmostEquals(await xUSDT.getUnitPrice(), ethers.parseEther("1"));
+      expectAlmostEquals(await xUSDT.getVaultValues(), ethers.parseEther("0"));
+      expectAlmostEquals(
+        await xUSDT.getVaultValuesInUSD(),
+        ethers.parseEther("0")
+      );
+
+      await xUSDT.deposit(defaultAmount);
+
+      expectAlmostEquals(await xUSDT.getUnitPrice(), ethers.parseEther("1"));
+      expectAlmostEquals(
+        await xUSDT.getVaultValues(),
+        ethers.parseEther("1000")
+      );
+      expectAlmostEquals(
+        await xUSDT.getVaultValuesInUSD(),
+        ethers.parseEther("1000")
+      );
+
+      await xUSDT.connect(accounts[0]).deposit(defaultAmount);
+
+      expectAlmostEquals(await xUSDT.getUnitPrice(), ethers.parseEther("1"));
+      expectAlmostEquals(
+        await xUSDT.getVaultValues(),
+        ethers.parseEther("2000")
+      );
+      expectAlmostEquals(
+        await xUSDT.getVaultValuesInUSD(),
+        ethers.parseEther("2000")
+      );
+
+      await xUSDT.systemDeposit();
+
+      await xUSDT.withdraw(await xUSDT.balanceOf(await owner.getAddress()));
+
+      expectAlmostEquals(await xUSDT.getUnitPrice(), ethers.parseEther("1"));
+      expectAlmostEquals(
+        await xUSDT.getVaultValues(),
+        ethers.parseEther("1000")
+      );
+      expectAlmostEquals(
+        await xUSDT.getVaultValuesInUSD(),
+        ethers.parseEther("1000")
+      );
+    });
+
+    it("xBTCB", async function () {
+      const { owner, accounts, xUSDT, xBTCB, USDT, BTCB } = await loadFixture(
+        xWinFixture
+      );
+      await BTCB.approve(await xBTCB.getAddress(), defaultAmountBTC);
+      await BTCB.connect(accounts[0]).approve(
+        await xBTCB.getAddress(),
+        defaultAmountBTC
+      );
+
+      expect(await xBTCB.baseToken()).to.equal(bsc.BTCB);
+      expectAlmostEquals(await xBTCB.getUnitPrice(), ethers.parseEther("1"));
+      expectAlmostEquals(await xBTCB.getVaultValues(), ethers.parseEther("0"));
+      expectAlmostEquals(
+        await xBTCB.getVaultValuesInUSD(),
+        ethers.parseEther("0")
+      );
+
+      await xBTCB.deposit(defaultAmountBTC);
+
+      expectAlmostEquals(await xBTCB.getUnitPrice(), ethers.parseEther("1"));
+      expectAlmostEquals(await xBTCB.getVaultValues(), defaultAmountBTC);
+      expectAlmostEquals(
+        await xBTCB.getVaultValuesInUSD(),
+        ethers.parseEther("4260")
+      );
+
+      await xBTCB.connect(accounts[0]).deposit(defaultAmountBTC);
+
+      expectAlmostEquals(await xBTCB.getUnitPrice(), ethers.parseEther("1"));
+      expectAlmostEquals(
+        await xBTCB.getVaultValues(),
+        ethers.parseEther("0.2")
+      );
+      expectAlmostEquals(
+        await xBTCB.getVaultValuesInUSD(),
+        ethers.parseEther("8520")
+      );
+
+      await xBTCB.systemDeposit();
+
+      await xBTCB.withdraw(await xBTCB.balanceOf(await owner.getAddress()));
+
+      expectAlmostEquals(await xBTCB.getUnitPrice(), ethers.parseEther("1"));
+      expectAlmostEquals(
+        await xBTCB.getVaultValues(),
+        ethers.parseEther("0.1")
+      );
+      expectAlmostEquals(
+        await xBTCB.getVaultValuesInUSD(),
+        ethers.parseEther("4260")
+      );
+    });
+  });
+});
