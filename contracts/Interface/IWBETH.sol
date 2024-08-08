6a98f47403a5593734ca19763d6c5ed38785f18d
Wade <wz.lim.00@gmail.com>
2024-08-08 19:39:01 +0800

    liquid-staking strategy (#6)

---
+++ contracts/Interface/IWBETH.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.19;
+
+interface IWBETH {
+    function exchangeRate() external view returns (uint256);
+
+    function deposit(uint256 amount, address referral) external;
+}
